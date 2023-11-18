3d714da941b0e6afc4c62dcbb701011b9a8cdc18
rtavaresdev <rtavares.adriano@gmail.com>
2023-11-18 18:33:11 -0300

    escrevendo-as-classes-de-um-jogo

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 class hero {
     constructor(nome, idade, tipo){
-        this.nome = "Capitão América";
-        this.idade = 8;
-        this.tipo = "ninja";
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
     }
 
     attack() {
         let attack;
         switch (this.tipo) {
             case "mago":
-                attack = "usou magia";
+                attack = "usou magia"
                 break;
             case "guerreiro":
-                attack = "usou espada";
+                attack = "usou espada"
                 break;
             case "monge":
-                attack = "usou artes marciais";
+                attack = "usou artes marciais"
                 break;
             case "ninja":
-                attack = "usou shuriken";
+                attack = "usou shuriken"
                 break;
             default:
-                attack = "usou um ataque indefinido";    
+                attack = "usou um ataque indefinido"
         }
-        console.log('O ${this.tipo} atacou usando ${attack}');    
+        console.log(`O ${this.tipo} atacou usando ${attack}`)  
         
     }
 
 }
+
+const hero1 = new hero ("Capitão América ", 100, "mago");
+hero1.attack();
+
+
